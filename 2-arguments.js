504b1d7c484bc17f2b21cbbbb3c3abaa162f35ba
meshackc0de <meshackjohn420@gmail.com>
2025-07-12 07:10:05 +0300

    error fix

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,17 +2,14 @@
 
 let days = [];
 
-if (days.length < 1){
+if (days < 1){
   console.log('No argument');
+} else if(days === 1){
+  console.log('Argument found');
+} else if (days > 1){
+  console.log('Arguments found');
+
 }
 
 
-  let names = ['Best'];
-  if(names.length === 1){
-    console.log('Argument found');
-  }
-
-  let result = ['best', 'School'];
-  if (result.length > 1 ){
-    console.log('Arguments found');
-  }
+ 
