213132e70473d6afcd7f24fae58794d4682e4bda
meshackc0de <meshackjohn420@gmail.com>
2025-07-12 06:35:40 +0300

    fix error

--- 2-arguments.js
+++ 2-arguments.js
@@ -6,7 +6,7 @@ if (days.length === 0){
   console.log('No argument');
 } else if (days.length === 1){
   console.log('Argument found');
-} else {
+} else if (days.length > 1){
   console.log('Arguments found');
 }
 
