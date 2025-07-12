89b030edfc760e511b72162899e09dfd672b4968
meshackc0de <meshackjohn420@gmail.com>
2025-07-12 06:50:43 +0300

    error fix

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,10 @@
 
 
-let days = ['Best','School'];
+let days = ['Best'];
 
-if (days.length === 0){
+if (days.length < 1){
   console.log('No argument');
-} else if (days.length === 1){
+} else if (days.length === 1 && days.length > 0){
   console.log('Argument found');
 } else if (days.length > 1){
   console.log('Arguments found');
