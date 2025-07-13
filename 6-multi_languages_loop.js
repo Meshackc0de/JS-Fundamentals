55bb59b6e5e509aee963cca08e8cdc4ed3a0d7c6
meshackc0de <meshackjohn420@gmail.com>
2025-07-13 20:34:41 +0300

    6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+//print 3 lines using an array of string and a loop
+
+const facts = ['C is fun', 'Python is cool','JavaScript is amazing'];
+
+for(let i = 0; i < facts.length; i++){
+  console.log(facts[i]);
+}
