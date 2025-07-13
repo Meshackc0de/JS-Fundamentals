8d9bad5d6fd4320fe9bd51081d13f29d5147f5ec
meshackc0de <meshackjohn420@gmail.com>
2025-07-13 22:53:35 +0300

    add 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,12 +1,12 @@
 const arg = parseInt(process.argv[2]);
 
-if (isNaN(arg) || arg < 0){
-  console.log(' Missing size');
-
-} else {
-  for (let i = 0; i < arg; i++){
+if (isNaN(arg)) {
+  console.log('Missing size');
+} else if (arg > 0) {
+  let i = 0;
+  while (i < arg) {
     console.log('X'.repeat(arg));
-
+    i++;
   }
 }
 
