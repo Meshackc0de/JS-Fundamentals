4e4a87e3d604e9c60aa19acf18994bd6a6a0c15c
meshackc0de <meshackjohn420@gmail.com>
2025-07-13 21:00:04 +0300

    add 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,9 +2,7 @@
 
 const facts = ['C is fun', 'Python is cool','JavaScript is amazing'];
 
-// for(let i = 0; i < facts.length; i++){
-//   console.log(facts[i]);
-// }
+
 let i = 0;
 let result = '';
 while(i < facts.length){
